4d837cb744b68f593c9fde8e4d72213b2c704960
Tshifhiwa <tdmadima@gmail.com>
2019-12-22 11:28:22 +0200

    'testing

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello this si a test is tshifhiwa, %s!", someone);
+    return String.format("Hello this si a test is tshifhiwa maven projects testing, %s!", someone);
   }
 }
